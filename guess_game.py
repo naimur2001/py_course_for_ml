38fb72dafb69cc40efec86ede622a5a955f02599
naimur2001 <naimurrahman18thapril@gmail.com>
2026-06-25 15:46:07 +0600

    guess game updated

--- guess_game.py
+++ guess_game.py
@@ -2,8 +2,8 @@ import random
 
 
 guess_number=random.randint(1,10)
-print("guess the number = ")
-guess=int(input())
+
+guess=int(input("guess the number = ") )
 guess_limit=1
 guess_count=0
 
@@ -12,7 +12,7 @@ while guess!=guess_number and guess_count<guess_limit:
     print("guess higher")
   else:
     print("guess lower")
-  guess=int(input())
+  guess=int(input("guess the number = "))
   guess_count+=1
 if guess==guess_number:
   print("you won")
